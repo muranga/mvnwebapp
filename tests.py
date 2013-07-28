0881107d6117b64c91121dfec516275f376845d0
James Muranga <jmured@gmail.com>
2013-07-28 17:56:00 +0300

    fixed the test

--- tests.py
+++ tests.py
@@ -8,7 +8,7 @@ class MainHandlerTest(unittest.TestCase):
 	request = webapp2.Request.blank('/')
 	response = request.get_response(main.app)
 	self.assertEqual(response.status_int,200)
-	self.assertEqual(response.body, 'Hello world!')
+	self.assertTrue( 'Hello world' in response.body)
 
 if __name__ == '__main__':
 	unittest.main()
